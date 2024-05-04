9535d54d3d1562ba74859cb260036a0c75e72b58
Kenil Jagani <keniljagani513@gmsil.com>
2024-05-04 09:07:46 +0530

    NA

--- Chapter_6/Recursion.py
+++ Chapter_6/Recursion.py
@@ -1,17 +1,17 @@
 # Recursive function
-# def show(n):
-#     if (n == 0):
-#         return
-#     print(n)
-#     show(n-1)
+def show(n):
+    if (n == 0):
+        return
+    print(n)
+    show(n-1)
 
-# show(5)
+show(5)
 
 # Factorial code
-def fact(n):
-    if (n == 0 or n == 1):
-        return 1
-    else:
-        return fact(n-1) * n
+# def fact(n):
+#     if (n == 0 or n == 1):
+#         return 1
+#     else:
+#         return fact(n-1) * n
 
-print(fact(5))
+# print(fact(5))
--- Chapter_6/practiceprog1.py
+++ Chapter_6/practiceprog1.py
@@ -5,14 +5,14 @@
 # print_len([1, 2, 3, 4, 5])
 
 # Prob 2: WAF to print the elements of a list in a single line (list is the parameter)
-# list = ["movie", "song", "Dance"]
-# num = [1, 2, 3, 4, 5]
+list = ["movie", "song", "Dance"]
+num = [1, 2, 3, 4, 5]
 
-# def print_list(list):
-#     for item in list:
-#         print(item, end=" ")
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
 
-# print_list(list)
+print_list(list)
 
 # Prob 3: WAF to find the factorial of n.(n is the parameter)
 # def fect_n(n):
@@ -33,10 +33,10 @@
 # converter(3)
 
 # Prob 5: WAP in which input is number and if it is odd then in return it give a string "ODD" anf if number is even then it will give "EVEN"
-def num(n):
-    if (n%2 != 0):
-        print(n, "is ODD number.")
-    else:
-        print(n, "is EVEN number.")
+# def num(n):
+#     if (n%2 != 0):
+#         print(n, "is ODD number.")
+#     else:
+#         print(n, "is EVEN number.")
 
-num(2)
+# num(2)
